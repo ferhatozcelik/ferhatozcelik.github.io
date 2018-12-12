296bd8eb5d424192c82fb9fcc96d40cc26df55de
Ferhat ÖZÇELİK <34388394+ferhat01@users.noreply.github.com>
2018-12-12 21:54:14 +0300

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Powered by Zyro.com
-if (version_compare(PHP_VERSION, '5.3.0') < 0) {
-	echo "Your PHP version is outdated for this website. Please update PHP version to 5.3.6 or higher.";
-	exit();
-}
-include dirname(__FILE__).'/zyro/index.php';
-?>
