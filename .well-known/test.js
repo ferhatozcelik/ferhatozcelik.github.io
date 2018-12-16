0e1e362d5059fa639ba3892d27a4708a5de0833b
Ferhat ÖZÇELİK <34388394+ferhatozcelik@users.noreply.github.com>
2018-12-16 04:43:41 +0300

    Create test.js

---
+++ .well-known/test.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/5.7.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBU0Cc5cpG8c7jpOM-1LtpaVA4TLrVHSEQ",
+    authDomain: "pubarea-e233e.firebaseapp.com",
+    databaseURL: "https://pubarea-e233e.firebaseio.com",
+    projectId: "pubarea-e233e",
+    storageBucket: "pubarea-e233e.appspot.com",
+    messagingSenderId: "97397499156"
+  };
+  firebase.initializeApp(config);
+</script>
