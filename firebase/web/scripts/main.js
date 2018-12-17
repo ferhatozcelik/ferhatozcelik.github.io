2e36a6293e2a73113ffcbb2df7804a40ca1ade43
Ferhat ÖZÇELİK <34388394+ferhatozcelik@users.noreply.github.com>
2018-12-18 02:40:35 +0300

    Update main.js

--- firebase/web/scripts/main.js
+++ firebase/web/scripts/main.js
@@ -82,7 +82,7 @@ function saveMessage(messageText) {
 // This first saves the image in Firebase storage.
 function saveImageMessage(file) {
   // 1 - We add a message with a loading icon that will get updated with the shared image.
-  firebase.database().ref('/messages/').push({
+  firebase.database().ref('/data/').push({
     name: getUserName(),
     imageUrl: LOADING_IMAGE_URL,
     profilePicUrl: getProfilePicUrl()
