9da1cf0176a9c05066f09cff838cc29341fd30e4
Ferhat ÖZÇELİK <34388394+ferhatozcelik@users.noreply.github.com>
2018-12-17 14:45:48 +0300

    Update main.js

--- firebase/web/scripts/main.js
+++ firebase/web/scripts/main.js
@@ -141,12 +141,7 @@ function onMediaFileSelected(event) {
   // Clear the selection in the file picker input.
   imageFormElement.reset();
 
-  // Check if the file is an image.
-  if (!file.type.match('image.*')) {
-    var data = {
-      message: 'You can only share images',
-      timeout: 2000
-    };
+  
     signInSnackbarElement.MaterialSnackbar.showSnackbar(data);
     return;
   }
