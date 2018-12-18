7ab364b4d11b699f80bc104f27669148054646c0
Ferhat ÖZÇELİK <34388394+ferhatozcelik@users.noreply.github.com>
2018-12-18 13:08:22 +0300

    Update main.js

--- firebase/web/scripts/main.js
+++ firebase/web/scripts/main.js
@@ -337,24 +337,3 @@ initFirebaseAuth();
 // We load currently existing chat messages and listen to new ones.
 loadMessages();
 
-
-
-var form = document.querySelector(“form”);
-form.addEventListener(“submit”, function(event) { 
-event.preventDefault();
-
-
-var timestamp = Number(new Date());
-var storageRef = firebase.storage().ref(timestamp.toString());
-
-var $ = jQuery;
-var file_data = $(‘#uploadZip’).prop(‘files’)[0];
-
-
-storageRef.put(file_data);
-
-
-
-
-
-
